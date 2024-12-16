619bf6274560fec9c2dbfab2b78f845c078ccb73
Filbert Mangiri <filbertmangiri12345@gmail.com>
2024-12-17 04:28:10 +0700

    prettier .mjs configuration

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+const config = {
+  semi: false,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 100,
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+}
+
+export default config
